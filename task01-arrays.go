43bc5e44b7421e4e73957309d864aea80de8c3f3
iNaz07 <ekrezan.94@mail.ru>
2022-05-11 00:54:25 +0600

    all

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,5 +2,13 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
-	return
+	var count float32
+	for _, num := range input {
+		if num != 0 {
+			result += num
+			count++
+		}
+	}
+	return result / count
+
 }
